b98836236be58217d3965de7e5664518bb625930
0ndra0 <130213334+0ndra0@users.noreply.github.com>
2023-10-14 14:13:13 +0200

    3.0.0-RC.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 1
-        versionName = "3.0.0-RC.2"
+        versionName = "3.0.0-RC.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
