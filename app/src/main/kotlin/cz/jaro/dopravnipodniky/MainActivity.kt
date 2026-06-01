60d8492938310bb018bcce27158f1f9305a790e3
rblaha15 <72446087+rblaha15@users.noreply.github.com>
2026-06-02 00:33:41 +0200

    Fixed offline simulation loading

--- app/src/main/kotlin/cz/jaro/dopravnipodniky/MainActivity.kt
+++ app/src/main/kotlin/cz/jaro/dopravnipodniky/MainActivity.kt
@@ -18,6 +18,7 @@ import androidx.compose.material.icons.filled.Euro
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
@@ -31,6 +32,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.pluralStringResource
 import androidx.compose.ui.res.stringResource
 import androidx.datastore.preferences.core.PreferenceDataStoreFactory
 import androidx.datastore.preferences.preferencesDataStoreFile
@@ -48,6 +50,8 @@ import cz.jaro.dopravnipodniky.migrations.NovySystemGeneratoruMigration
 import cz.jaro.dopravnipodniky.shared.SharedViewModel
 import cz.jaro.dopravnipodniky.shared.StavHry
 import cz.jaro.dopravnipodniky.shared.StavTutorialu
+import cz.jaro.dopravnipodniky.shared.composeString
+import cz.jaro.dopravnipodniky.shared.formatovat
 import cz.jaro.dopravnipodniky.shared.je
 import cz.jaro.dopravnipodniky.shared.stavHry
 import cz.jaro.dopravnipodniky.shared.zpomalit
@@ -65,6 +69,7 @@ import org.koin.compose.KoinApplication
 import org.koin.compose.koinInject
 import org.koin.dsl.module
 import kotlin.time.Duration
+import kotlin.time.Duration.Companion.hours
 import kotlin.time.Duration.Companion.milliseconds
 import kotlin.time.Duration.Companion.seconds
 
@@ -247,50 +252,14 @@ class MainActivity : ComponentActivity() {
 
                     if (stavHry is StavHry.Simulace && vyuctovani != null && !zobrazitLoading) AlertDialog(
                         onDismissRequest = {},
-                        confirmButton = {/*
-                        TextButton(
-                            onClick = {
-                                vyuctovani = null
-                            }
-                        ) {
-                            Text(stringResource(android.R.string.ok))
-                        }
-                    */
-                        },
-                        icon = {
-                            Icon(Icons.Default.Euro, null)
-                        },
-                        title = {
-                            Text(stringResource(R.string.slovo_vyuctovani))
-                        },
+                        confirmButton = {},
+                        icon = { Icon(Icons.Default.Euro, null) },
+                        title = { Text(stringResource(R.string.slovo_vyuctovani)) },
                         text = {
-                            Column {
-                                /*val stav = stavHry as StavHry.Simulace TODO
+                            if (stavHry is StavHry.Simulace && vyuctovani != null && !zobrazitLoading) Column {
+                                val stav = stavHry as StavHry.Simulace
                                 val celkem = vyuctovani!!
                                 Text(
-//                            stringResource(
-//                                R.string.vyuctovani,
-//                                if (vyuctovani!! < 2.hours)
-//                                    pluralStringResource(
-//                                        R.plurals.min,
-//                                        vyuctovani!!.inWholeMinutes.toInt(),
-//                                        vyuctovani!!.inWholeMinutes,
-//                                    )
-//                                else
-//                                    pluralStringResource(
-//                                        R.plurals.hod,
-//                                        vyuctovani!!.inWholeHours.toInt(),
-//                                        vyuctovani!!.inWholeHours,
-//                                    ),
-//                                (dpInfo!!.zisk * nasobitelZiskuPoOffline * vyuctovani!!).asString(),
-//                                (dpInfo!!.zisk).asString(),
-//                                vyuctovani!!.inWholeMinutes.formatovat(0).composeString(),
-//                                nasobitelZiskuPoOffline.times(100).formatovat(0).composeString(),
-//                                (dpInfo!!.zisk * nasobitelZiskuPoOffline * vyuctovani!!).asString(),
-//                                if (vyuctovani!! >= 8.hours)
-//                                    getString(R.string.bohuzel_dlouho_neaktivni)
-//                                else ""
-//                            ),
                                     stringResource(
                                         R.string.vyuctovani,
                                         if (celkem < 2.hours)
@@ -317,16 +286,13 @@ class MainActivity : ComponentActivity() {
                                                 (celkem - stav.zbyva).inWholeHours.toInt(),
                                                 (celkem - stav.zbyva).inWholeHours,
                                             ),
-                                        (1 - stav.zbyva / celkem).times(100).formatovat(0)
-                                            .composeString(),
+                                        (1 - stav.zbyva / celkem).times(100).formatovat(0).composeString(),
                                     ),
                                     Modifier.verticalScroll(rememberScrollState()),
                                 )
-                                LinearProgressIndicator(
-                                    progress = {
-                                        (1 - stav.zbyva / celkem).toFloat()
-                                    },
-                                )*/
+                                LinearProgressIndicator(progress = {
+                                    (1 - stav.zbyva / celkem).toFloat()
+                                })
                             }
                         },
                     )
